d1b2f5c15788cbb12f7dd1692240099682b6d22e
Bilkis Ismail <bilkisismail07@gmail.com>
2023-06-19 10:16:39 +0700

    [add] google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,35 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/index.scss";
 
+import Script from "next/script";
 import { Provider } from "react-redux";
 import { store } from "@/store"
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-7G8S2BB6LG"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-7G8S2BB6LG', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
+
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </>
   );
 }
